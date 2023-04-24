2d000ceb0acbb5169040493693d23b975312bed2
dickloraine <dickloraine@gmx.net>
2023-04-24 14:22:28 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dickloraine.github.io/xenos-rampant-builder/sw.js', { scope: '/dickloraine.github.io/xenos-rampant-builder/' })})}
