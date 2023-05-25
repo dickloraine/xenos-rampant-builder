37450ee3a28024714c798d8a75a200a1b2067b59
dickloraine <dickloraine@gmx.net>
2023-05-25 09:29:10 +0000

    stop re-rendering on roster changes

--- src/components/MenuActions/SaveList.tsx
+++ src/components/MenuActions/SaveList.tsx
@@ -1,8 +1,10 @@
 import SaveIcon from '@mui/icons-material/Save';
 import React from 'react';
-import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
+import { useStore } from 'react-redux';
+import { useAppDispatch } from '../../hooks/reduxHooks';
 import { showFeedback } from '../../store/appStateSlice';
 import { rosterStore } from '../../store/persistantStorage';
+import { AppStore } from '../../store/store';
 import { packRoster } from '../Roster';
 import MenuAction from './MenuAction';
 
@@ -11,9 +13,10 @@ const SaveList: React.FC<{ onClose?: () => void; showText?: boolean }> = ({
   showText,
 }) => {
   const dispatch = useAppDispatch();
-  const roster = useAppSelector((state) => state.roster);
+  const store: AppStore = useStore();
 
   const saveRoster = () => {
+    const roster = store.getState().roster;
     if (roster.name === 'New List') {
       dispatch(showFeedback('You have to give the list a name!', 'error'));
       return;
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -15,6 +15,8 @@ export const store = configureStore({
   reducer: rootReducer,
 });
 
+export type AppStore = typeof store;
+
 export type RootState = ReturnType<typeof rootReducer>;
 
 export type AppDispatch = typeof store.dispatch;
